026ed1197f61e5dfe5468990842b2bcc465e6d50
keob <isgopher@outlook.com>
2020-09-23 14:51:37 +0800

    Add convert bst to greater tree solution

---
+++ solution538/solution.js
@@ -0,0 +1,27 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const convertBST = function (root) {
+  const convert = node => {
+    if (node === null) {
+      return 0;
+    };
+
+    convert(node.right);
+    sum += node.val;
+    node.val = sum;
+    convert(node.left);
+  }
+
+  let sum = 0;
+
+  convert(root);
+
+  return root;
+};

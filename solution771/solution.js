7349be44a48d87a95544f9e70f4cdcf9513d8108
keob <isgopher@outlook.com>
2020-10-02 09:27:22 +0800

    Add jewels and stones solution

---
+++ solution771/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+const numJewelsInStones = function (J, S) {
+  let j = J.split("");
+  let s = s.split("");
+
+  return s.filter(item => j.includes(item)).length;
+}

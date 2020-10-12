eda1c3c63ea93208d8bedf875b6249b051865613
keob <isgopher@outlook.com>
2020-10-12 19:07:33 +0800

    Add minimum absolute difference in bst solution

---
+++ solution530/solution.js
@@ -0,0 +1,30 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const getMinimumDifference = function (root) {
+  let ans = Number.MAX_SAFE_INTEGER, pre = -1;
+
+  const dfs = (root) => {
+    if (root === null) {
+      return;
+    }
+    dfs(root.left);
+    if (pre == -1) {
+      pre = root.val;
+    } else {
+      ans = Math.min(ans, root.val - pre);
+      pre = root.val;
+    }
+    dfs(root.right);
+  }
+
+  dfs(root);
+
+  return ans;
+};

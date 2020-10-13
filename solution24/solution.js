fe70cdf672d2f1adac8ce5ee7ab1ca5257e90136
keob <isgopher@outlook.com>
2020-10-13 20:21:03 +0800

    Add swap nodes in pairs solution

---
+++ solution24/solution.js
@@ -0,0 +1,30 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = function (head) {
+  const dummyHead = new ListNode(0);
+  dummyHead.next = head;
+
+  let prev = dummyHead;
+  let first = prev.next;
+
+  while (first && first.next) {
+    let second = first.next;
+    let next = second.next;
+
+    second.next = first;
+    first.next = next;
+    prev.next = second;
+
+    prev = first;
+    first = first.next;
+  }
+
+  return dummyHead.next;
+};

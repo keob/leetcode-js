5456aa3742d71f19956f18a542c6ca605eb88c4e
keob <isgopher@outlook.com>
2020-10-13 20:23:51 +0800

    Add solution

---
+++ solution24/solution2.js
@@ -0,0 +1,21 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = function (head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+
+  const newHead = head.next;
+
+  head.next = swapPairs(newHead.next);
+  newHead.next = head;
+
+  return newHead; F
+};

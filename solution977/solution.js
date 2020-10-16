fd7c302a736b8f711b556a92567f399410f8924a
keob <isgopher@outlook.com>
2020-10-16 19:55:06 +0800

    Add squares of a sorted array solution

---
+++ solution977/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+const sortedSquares = function (A) {
+  const len = A.length, res = new Array(len);
+  let left = 0, right = len - 1;
+
+  while (left <= right) {
+    if (A[left] < 0 && -A[left] > A[right]) {
+      res[right - left] = A[left++] ** 2;
+    } else {
+      res[right - left] = A[right--] ** 2;
+    }
+  }
+
+  return res;
+}

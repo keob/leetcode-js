70f1f089228be34d8c85ddd30cc563001bba35f3
keob <isgopher@outlook.com>
2020-10-19 19:17:59 +0800

    Add backspace string compare solution

---
+++ solution844/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+const backspaceCompare = function (S, T) {
+  const c = (s, a = []) => {
+    for (let i = 0; i < s.length; i++) {
+      s[i] === '#' ? a.pop() : a.push(s[i])
+    }
+    return a.join('')
+  }
+  return c(S) === c(T)
+};

1802dd3b9af1560e69faf684429569e759814114
keob <isgopher@outlook.com>
2020-10-23 19:16:39 +0800

    Add palindrome linked list solution

---
+++ solution234/solution.js
@@ -0,0 +1,25 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = function (head) {
+  const vals = [];
+
+  while (head !== null) {
+    vals.push(head.val);
+    head = head.next;
+  }
+
+  for (let i = 0, j = vals.length - 1; i < j; ++i, --j) {
+    if (vals[i] !== vals[j]) {
+      return false;
+    }
+  }
+
+  return true;
+}

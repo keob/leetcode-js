52403650ff469c2c4e07305a1440e722617f0430
keob <isgopher@outlook.com>
2020-10-25 18:53:03 +0800

    Add longest mountain in array solution

---
+++ solution845/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const longestMountain = function (A) {
+  let res = 0;
+
+  for (let i = 1; i < A.length - 1; i++) {
+    if (A[i] > A[i - 1] && A[i] > A[i + 1]) {
+      let len = i;
+      let count = 1;
+      while (A[len] > A[len - 1] && len > 0) {
+        count++;
+        len--;
+      }
+      len = i;
+      while (A[len] > A[len + 1] && len < A.length) {
+        count++;
+        len++;
+      }
+      res = res > count ? res : count;
+    }
+  }
+
+  return res;
+};

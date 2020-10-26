5161fdc234847ef2353b1f69a849bda6102a7dfb
keob <isgopher@outlook.com>
2020-10-26 18:37:29 +0800

    Add how many numbers are smaller than the current number solution

---
+++ solution1365/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const smallerNumbersThanCurrent = function (nums) {
+  const cnt = new Array(101).fill(0);
+  const n = nums.length;
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    cnt[nums[i]] += 1;
+  }
+
+  for (let i = 1; i <= 100; i++) {
+    cnt[i] += cnt[i - 1];
+  }
+
+  for (let i = 0; i < n; i++) {
+    res.push(nums[i] ? cnt[nums[i] - 1] : 0);
+  }
+
+  return res;
+}
